08d1ae9fe26546e44158abfe7d5cd373327e2fb5
chofame <chofame@naver.com>
2022-05-22 03:03:37 +0900

    Sign up Page

---
+++ Travel/app/src/main/java/com/example/travel/SignUp/SignUpActivity.java
@@ -0,0 +1,17 @@
+package com.example.travel.SignUp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.travel.R;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+    }
+
+}

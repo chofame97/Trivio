7f348874f76357f49d519cdbc52921947d786ef5
chofame <chofame@naver.com>
2022-06-25 17:48:39 +0900

    update

---
+++ Trivio/src/main/java/go/travel/trivio/domain/member/dto/Member.java
@@ -0,0 +1,9 @@
+package go.travel.trivio.domain.member.dto;
+
+import lombok.Data;
+
+@Data
+public class Member {
+    private int id;
+
+}
---
+++ Trivio/src/test/java/go/travel/trivio/TrivioApplicationTests.java
@@ -0,0 +1,13 @@
+package go.travel.trivio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrivioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
